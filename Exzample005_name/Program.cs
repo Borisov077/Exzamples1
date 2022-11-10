979113e150cc00f32e821e26c20891223e5ebf35
Виталий Борисов <goldstar_0990@mail.ru>
2022-11-10 22:14:01 +0100

    add Exzampel_005_name

---
+++ Exzample005_name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+String username = Console.ReadLine();
+
+if (username.ToLower() == "vitalii")
+{
+    Console.WriteLine("УРА - это Vitalii");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
